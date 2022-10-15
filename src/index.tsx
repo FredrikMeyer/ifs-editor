306965b601f4fac01a339f609103cf3725545e3e
Fredrik Meyer <hrmeyer@gmail.com>
2022-10-15 14:12:27 +0200

    Bump MUI to v5

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./style.css";
-
 import App from "./App";
 
+const theme = createMuiTheme();
+
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  mountNode
+);
