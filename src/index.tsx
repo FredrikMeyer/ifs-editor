e959d83f63445b6efa674af28bfb1545566c4510
Fredrik Meyer <hrmeyer@gmail.com>
2022-11-07 21:01:15 +0100

    Allow for several tests at the same time, only one deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import {
-  ThemeProvider,
-  StyledEngineProvider,
-  createTheme,
-} from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import "./style.css";
 import App from "./App";
