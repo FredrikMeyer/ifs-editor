c07040b81605ce70348e127631cc2f6c807a726c
Fredrik Meyer <hrmeyer@gmail.com>
2023-01-29 20:50:37 +0100

    Link to latest commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,38 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { exec } from "child_process";
+import { promisify } from "util";
+
+async function getCommitHash() {
+  const execP = promisify(exec);
+
+  const { stdout } = await execP("git rev-parse --short HEAD");
+
+  return stdout;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: "./docs",
-  },
-  base: "/ifs-editor/",
-  plugins: [react()],
-  test: {
-    includeSource: ["src/**/*.ts*"],
-    coverage: {
-      reporter: ["text", "html"],
+export default defineConfig(async ({ command, mode }) => {
+  const commitHash = await getCommitHash();
+
+  console.log("CCCC", commitHash);
+  return {
+    build: {
+      outDir: "./docs",
     },
-  },
-  define: {
-    "import.meta.vitest": false,
-  },
+    base: "/ifs-editor/",
+    plugins: [react()],
+    test: {
+      includeSource: ["src/**/*.ts*"],
+      coverage: {
+        reporter: ["text", "html"],
+      },
+    },
+    define: {
+      "import.meta.vitest": false,
+      __COMMIT_HASH__: JSON.stringify(commitHash),
+    },
+  };
 });
